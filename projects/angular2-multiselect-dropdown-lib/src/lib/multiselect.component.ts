4bc63ad8ee08adf69f5ccaad0e7b8394d7162f91
Paixao Marques <73987@entourage.intra>
2024-04-24 11:31:46 -0400

    angular 17

--- projects/angular2-multiselect-dropdown-lib/src/lib/multiselect.component.ts
+++ projects/angular2-multiselect-dropdown-lib/src/lib/multiselect.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit, HostListener, ChangeDetectionStrategy, OnDestroy, NgModule, SimpleChanges, OnChanges, ChangeDetectorRef, AfterViewChecked, ViewEncapsulation, ContentChild, ViewChild, forwardRef, Input, Output, EventEmitter, ElementRef, AfterViewInit, Pipe, PipeTransform } from '@angular/core';
-import { FormsModule, NG_VALUE_ACCESSOR, ControlValueAccessor, NG_VALIDATORS, Validator, FormControl } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-import { MyException } from './multiselect.model';
-import { DropdownSettings } from './multiselect.interface';
-import { ClickOutsideDirective, ScrollDirective, styleDirective, setPosition } from './clickOutside';
-import { ListFilterPipe } from './list-filter';
-import { Item, Badge, Search, TemplateRenderer, CIcon } from './menu-item';
-import { DataService } from './multiselect.service';
-import { Subscription, Subject } from 'rxjs';
-import { VirtualScrollerModule, VirtualScrollerComponent } from './virtual-scroll/virtual-scroll';
-import { map, debounceTime, distinctUntilChanged, switchMap, tap } from 'rxjs/operators';
+import { CommonModule } from "@angular/common";
+import { forwardRef, Component, ViewEncapsulation, OnInit, OnChanges, AfterViewChecked, OnDestroy, Input, Output, EventEmitter, ContentChild, ViewChild, ElementRef, HostListener, ChangeDetectorRef, SimpleChanges, NgModule } from "@angular/core";
+import { NG_VALUE_ACCESSOR, NG_VALIDATORS, ControlValueAccessor, Validator, UntypedFormControl, FormsModule } from "@angular/forms";
+import { Subject, Subscription, debounceTime, distinctUntilChanged, tap } from "rxjs";
+import { ClickOutsideDirective, ScrollDirective, styleDirective, setPosition } from "./clickOutside";
+import { ListFilterPipe } from "./list-filter";
+import { Item, Badge, Search, TemplateRenderer, CIcon } from "./menu-item";
+import { DropdownSettings } from "./multiselect.interface";
+import { MyException } from "./multiselect.model";
+import { DataService } from "./multiselect.service";
+import { VirtualScrollerComponent, VirtualScrollerModule } from "./virtual-scroll/virtual-scroll";
+
+
+// import { Component, OnInit, HostListener, ChangeDetectionStrategy, OnDestroy, NgModule, SimpleChanges, OnChanges, ChangeDetectorRef, AfterViewChecked, ViewEncapsulation, ContentChild, ViewChild, forwardRef, Input, Output, EventEmitter, ElementRef, AfterViewInit, Pipe, PipeTransform } from '@angular/core';
+// import { FormsModule, NG_VALUE_ACCESSOR, ControlValueAccessor, NG_VALIDATORS, Validator, UntypedFormControl } from '@angular/forms';
+// import { CommonModule } from '@angular/common';
+// import { MyException } from './multiselect.model';
+// import { DropdownSettings } from './multiselect.interface';
+// import { ClickOutsideDirective, ScrollDirective, styleDirective, setPosition } from './clickOutside';
+// import { ListFilterPipe } from './list-filter';
+// import { Item, Badge, Search, TemplateRenderer, CIcon } from './menu-item';
+// import { DataService } from './multiselect.service';
+// import { Subscription, Subject } from 'rxjs';
+// import { VirtualScrollerModule, VirtualScrollerComponent } from './virtual-scroll/virtual-scroll';
+// import { map, debounceTime, distinctUntilChanged, switchMap, tap } from 'rxjs/operators';
 
 export const DROPDOWN_CONTROL_VALUE_ACCESSOR: any = {
     provide: NG_VALUE_ACCESSOR,
@@ -292,7 +305,7 @@ export class AngularMultiSelect implements OnInit, ControlValueAccessor, OnChang
 
 
     }
-    public validate(c: FormControl): any {
+    public validate(c: UntypedFormControl): any {
         return null;
     }
     private onTouchedCallback: (_: any) => void = noop;
--- projects/angular2-multiselect-dropdown-lib/src/public-api.ts
+++ projects/angular2-multiselect-dropdown-lib/src/public-api.ts
@@ -1,6 +1,12 @@
+/*
+ * Public API Surface of angular2-multiselect-dropdown-lib
+ */
 export { AngularMultiSelect } from './lib/multiselect.component';
 export { ClickOutsideDirective,ScrollDirective,styleDirective,setPosition } from './lib/clickOutside';
 export { ListFilterPipe } from './lib/list-filter';
 export { Item } from './lib/menu-item';
 export { TemplateRenderer,Badge,Search,CIcon } from './lib/menu-item';
 export { AngularMultiSelectModule } from './lib/multiselect.component';
+
+// export * from './lib/angular2-multiselect-dropdown-lib.service';
+// export * from './lib/angular2-multiselect-dropdown-lib.component';
